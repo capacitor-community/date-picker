706fbd71bd432f6038da14ccea98bb3caf005679
Daniel Pereira <daniel.prr.azevedo@gmail.com>
2020-07-05 14:09:24 -0300

    chore: create functions call in app

--- demo/src/app/app.component.ts
+++ demo/src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 @Component({
   selector: 'app-root',
@@ -10,18 +8,14 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+
   constructor(
     private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
+    this.platform.ready().then(() => { });
   }
 }
--- demo/src/app/home/home.page.ts
+++ demo/src/app/home/home.page.ts
@@ -1,12 +1,111 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Plugins } from "@capacitor/core";
+import { DatePickerPluginInterface, DatePickerOptions, DatePickerTheme, DatePickerMode } from "@capacitor-community/date-picker/src";
+
+const DatePicker: DatePickerPluginInterface = Plugins.DatePickerPlugin as any;
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
 export class HomePage {
+  max: Date;
+  min: Date;
+  theme: DatePickerTheme;
+  mode: DatePickerMode;
+  locale: string;
+  doneText: string;
+  cancelText: string;
 
+  themeList: Array<{ value: DatePickerTheme; label: string }> = [
+    {
+      value: '',
+      label: 'Default Theme'
+    },
+    {
+      value: "dark",
+      label: "Material Dark Theme",
+    },
+    {
+      value: "light",
+      label: "Material Light Theme",
+    },
+    {
+      value: "legacyDark",
+      label: "Legacy Dark Theme",
+    },
+    {
+      value: "legacyLight",
+      label: "Legacy Light Theme",
+    },
+    {
+      value: "MyCustomLightDatePicker",
+      label: "My Custom Theme",
+    },
+  ];
+
+  modesList: Array<{ value: DatePickerMode; label: string }> = [
+    {
+      value: 'date',
+      label: 'Date'
+    },
+    {
+      value: 'time',
+      label: 'Time'
+    },
+    {
+      value: 'dateAndTime',
+      label: 'Date and Time'
+    }
+  ]
   constructor() {}
 
+  async maxFocus() {
+    this.max = null;
+    const pickerResult = await this.openPicker()
+    if (pickerResult?.value) {
+      this.max = new Date(pickerResult.value);
+    }
+  }
+
+  async minFocus() {
+    this.min = null;
+    const pickerResult = await this.openPicker()
+    if (pickerResult?.value) {
+      this.min = new Date(pickerResult.value);
+    }
+  }
+
+  async openPicker() {
+    const options: DatePickerOptions = {}
+    if (this.max) {
+      if (this.mode === 'date') {
+        this.max.setHours(23,59,59,999);
+      }
+      options.max = this.max.toISOString();
+    }
+    if (this.min) {
+      if (this.mode === 'date') {
+        this.min.setHours(0,0,0,0);
+      }
+      options.min = this.min.toISOString();
+    }
+    if (this.theme) {
+      options.theme = this.theme;
+    }
+    if (this.mode) {
+      options.mode = this.mode;
+    }
+    if (this.locale) {
+      options.locale = this.locale;
+    }
+    if (this.doneText) {
+      options.doneText = this.doneText;
+    }
+    if (this.cancelText) {
+      options.cancelText = this.cancelText;
+    }
+    return DatePicker.present(options)
+  }
 }
