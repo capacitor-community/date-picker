feaa295d5fdc9e6b1aa6ccf46e36f2a27c5a5dda
Daniel Pereira <daniel.prr.azevedo@gmail.com>
2020-07-05 14:07:06 -0300

    fix: docs on plugin interface

--- src/definitions.ts
+++ src/definitions.ts
@@ -5,23 +5,85 @@ declare module "@capacitor/core" {
 }
 
 export type DatePickerMode = 'time' | 'date' | 'dateAndTime' | 'countDownTimer';
-export type DatePickerTheme = 'light' | 'dark';
-export type DatePickerType = 'spinner' | 'calendar';
+export type DatePickerTheme = 'light' | 'dark' | 'legacyLight' | 'legacyDark' | string;
 
 export interface DatePickerOptions {
-  format?: string; // default: MM/dd/yyyy hh:mm a
-  locale?: string; // default: en_EN (only ios)
-  date?: string; // default: Date()
-  mode?: DatePickerMode; // default: dateAndTime
-  theme?: DatePickerTheme; // default: light
-  background?: string; // default: transparent (only ios)
-  type?: DatePickerType; // default: spinner (only android)
-  timezone?: string; // default: UTC (only android)
-  title?: string;
+  /**
+   * @type {string}
+   * @default "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+   * 
+   * @description ISO String format
+   */
+  format?: string;
+  /**
+   * @type {string}
+   * @defalt null
+   * 
+   * @description If null, empty or undefined, use the device locale
+   */
+  locale?: string;
+  /**
+   * @type {string}
+   * @default null
+   * 
+   * @description If null, empty or undefined, will be current date
+   */
+  date?: string;
+  /**
+   * @type {DatePickerMode}
+   * @default "dateAndTime"
+   * 
+   * @description Datepicker modes
+   */
+  mode?: DatePickerMode;
+  /**
+   * @type {DatePickerTheme}
+   * @default "light"
+   * 
+   * @description Datepicker themes
+   */
+  theme?: DatePickerTheme;
+  /**
+   * @type: string
+   * @default: null
+   * 
+   * @description If null, empty or undefined, will be the device timezone
+   */
+  timezone?: string;
+  /**
+   * @type {string}
+   * @default null
+   * 
+   * @description If null, empty or undefined, will be none. use Date.toISOString()
+   */
   min?: string;
+  /**
+   * @type {string}
+   * @default null
+   * 
+   * @description If null, empty or undefined, will be none. use Date.toISOString()
+   */
   max?: string;
+  /**
+   * @type {string}
+   * @default null
+   * 
+   * @description If null, empty or undefined, will be "OK".
+   */
   doneText?: string;
+  /**
+   * @type {string}
+   * @default null
+   * 
+   * @description If null, empty or undefined, will be "CANCEL".
+   */
   cancelText?: string;
+  /**
+   * @type {string}
+   * @default null
+   * 
+   * @description If null, empty or undefined, will be "false".
+   */
   is24h?: boolean; // only android
 }
 
