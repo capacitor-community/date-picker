c6fe81c479b964ad3c44b6fbdc403dd8efd87d4c
Daniel Pereira <daniel.prr.azevedo@gmail.com>
2021-02-12 19:24:01 -0300

    chore: selector android

--- android/src/main/java/com/getcapacitor/community/datepicker/DatePickerPlugin.java
+++ android/src/main/java/com/getcapacitor/community/datepicker/DatePickerPlugin.java
@@ -1,6 +1,5 @@
 package com.getcapacitor.community.datepicker;
 
-
 import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
@@ -9,27 +8,30 @@ import com.getcapacitor.annotation.CapacitorPlugin;
 
 @CapacitorPlugin(name = "DatePicker")
 public class DatePickerPlugin extends Plugin {
-    public static final String PLUGIN_PREFIX = "plugins.DatePickerPlugin.";
 
-    public DatePickerPlugin() { }
+    public static final String PLUGIN_PREFIX = "plugins.DatePicker.";
+
+    public DatePickerPlugin() {}
 
     @PluginMethod
     public void present(final PluginCall call) throws java.text.ParseException {
         DatePickerOptions options = new DatePickerOptions(PLUGIN_PREFIX, getConfig(), call);
         DatePicker datePicker = new DatePicker(options, getContext());
 
-        datePicker.open(new DatePickerResolve() {
-            @Override
-            public void resolve(String date) {
-                final JSObject response = new JSObject();
-                response.put("value", date);
-                call.resolve(response);
-            }
+        datePicker.open(
+            new DatePickerResolve() {
+                @Override
+                public void resolve(String date) {
+                    final JSObject response = new JSObject();
+                    response.put("value", date);
+                    call.resolve(response);
+                }
 
-            @Override
-            public void reject(String message) {
-                call.reject(message);
+                @Override
+                public void reject(String message) {
+                    call.reject(message);
+                }
             }
-        });
+        );
     }
 }
