01c3d6e528ee7fd6b85fb6981a47a4a28c2149d7
Daniel Pereira <daniel.prr.azevedo@gmail.com>
2021-02-12 02:05:41 -0300

    chore: create title view

--- ios/Plugin/DatePicker.swift
+++ ios/Plugin/DatePicker.swift
@@ -1,7 +1,23 @@
 import Foundation
+import Capacitor
 
-@objc public class DatePicker: NSObject {
-    @objc public func echo(_ value: String) -> String {
-        return value
+public class DatePicker: NSObject {
+    
+    private var options = DatePickerOptions()
+    
+    init(options: DatePickerOptions) {
+        self.options = options
+    }
+    
+    public func titleView() {
+        let title = UILabel(frame: CGRect(x: 0, y: 0, width: 10))
+        title.textAlignment = .center
+        title.text = self.titleChange(self.parseDateFromString(date: self.pickerDate!))
+        title.textColor = UIColor(hexString: self.pickerTitleFontColor)
+        title.backgroundColor = UIColor(hexString: self.pickerTitleBgColor)
+    }
+    
+    public func open(options: DatePickerOptions, completion: @escaping (_ value: String) -> Void) {
+        completion(options.title!)
     }
 }
