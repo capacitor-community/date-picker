ec9fb3bf65ad78b3d99c30e93edffcda3d802403
Daniel Pereira <daniel.prr.azevedo@gmail.com>
2021-02-12 02:04:26 -0300

    chore: add date parsers

---
+++ ios/Plugin/Parse.swift
@@ -0,0 +1,29 @@
+//
+//  Parse.swift
+//  Plugin
+//
+//  Created by Daniel Rosa on 12/02/21.
+//  Copyright © 2021 Max Lynch. All rights reserved.
+//
+
+import Foundation
+public class Parse {
+    public static func dateFromString(date: String, format: String? = nil, timezone: String? = nil) -> Date {
+        let formatter = DateFormatter()
+        if (format != nil) {
+            formatter.dateFormat = format;
+        }
+        if (timezone != nil) {
+            let tz = TimeZone(identifier: timezone ?? "UTC")
+            formatter.timeZone = tz;
+        }
+        return formatter.date(from: date)!;
+    }
+    public static func dateToString(date: Date, format: String? = nil) -> String {
+        let formatter = DateFormatter()
+        if (format != nil) {
+            formatter.dateFormat = format
+        }
+        return formatter.string(from: date)
+    }
+}
