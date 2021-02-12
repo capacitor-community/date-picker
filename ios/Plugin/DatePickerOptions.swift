e47f1a21747ce3a40a42a14502b9bb8434ac5694
Daniel Pereira <daniel.prr.azevedo@gmail.com>
2021-02-12 02:04:48 -0300

    chore: create options classes

---
+++ ios/Plugin/DatePickerOptions.swift
@@ -0,0 +1,56 @@
+//
+//  DatePickerOptions.swift
+//  Plugin
+//
+//  Created by Daniel Rosa on 12/02/21.
+//  Copyright © 2021 Max Lynch. All rights reserved.
+//
+
+import Foundation
+import Capacitor
+
+public class DatePickerOptions: NSObject, NSCopying {
+    public func copy(with zone: NSZone? = nil) -> Any {
+        let copy = DatePickerOptions()
+        copy.theme = theme
+        copy.mode = mode
+        copy.format = format
+        copy.timezone = timezone
+        copy.locale = locale
+        copy.cancelText = cancelText
+        copy.doneText = doneText
+        copy.is24h = is24h
+        copy.date = date
+        copy.min = min
+        copy.max = max
+        copy.title = title
+        copy.titleFontColor = titleFontColor
+        copy.titleBgColor = titleBgColor
+        copy.bgColor = bgColor
+        copy.fontColor = fontColor
+        copy.buttonBgColor = buttonBgColor
+        copy.buttonFontColor = buttonFontColor
+        copy.mergedDateAndTime = mergedDateAndTime
+        return copy;
+    }
+    
+    public var theme: String = "light"
+    public var mode: String = "dateAndTime"
+    public var format: String = "yyyy-MM-dd'T'HH:mm:ss.sss'Z'"
+    public var timezone: String? = nil
+    public var locale: String? = nil
+    public var cancelText: String = "Cancel"
+    public var doneText: String = "Ok"
+    public var is24h: Bool =  false
+    public var date: Date? = nil
+    public var min: Date? = nil
+    public var max: Date? = nil
+    public var title: String? = nil
+    public var titleFontColor: String = "#000000"
+    public var titleBgColor: String = "#ffffff"
+    public var bgColor: String = "#ffffff"
+    public var fontColor: String = "#000000"
+    public var buttonBgColor: String = "#ffffff"
+    public var buttonFontColor: String = "#000000"
+    public var mergedDateAndTime: Bool = false
+}
