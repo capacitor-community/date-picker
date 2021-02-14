aa5eaa3206936d03cacb0a57b88eba63ca21dcf1
Daniel Pereira <daniel.prr.azevedo@gmail.com>
2021-02-14 00:39:38 -0300

    chore: change internal logic for get configs in android

--- android/src/main/java/com/getcapacitor/community/datepicker/DatePickerOptions.java
+++ android/src/main/java/com/getcapacitor/community/datepicker/DatePickerOptions.java
@@ -1,12 +1,8 @@
 package com.getcapacitor.community.datepicker;
 
-import com.getcapacitor.PluginCall;
-import com.getcapacitor.PluginConfig;
-
-import java.text.ParseException;
 import java.util.Date;
 
-public class DatePickerOptions {
+public class DatePickerOptions implements Cloneable {
     public String format;
     public String locale;
     public Date date;
@@ -20,36 +16,15 @@ public class DatePickerOptions {
     public Boolean is24h;
     public String title;
 
-    public DatePickerOptions(final String prefix, PluginConfig config, PluginCall call) throws ParseException {
-        theme = config.getString(prefix + "theme", "light");
-        mode = config.getString(prefix + "mode", "dateAndTime");
-        format = config.getString(prefix + "format", "yyyy-MM-dd'T'HH:mm:ss.sss'Z'");
-        timezone = config.getString(prefix + "timezone", "UTC");
-        locale = config.getString(prefix + "locale",null);
-        cancelText = config.getString(prefix + "cancelText", null);
-        doneText = config.getString(prefix + "doneText",null);
-        is24h = config.getBoolean(prefix + "is24h", false);
+    public DatePickerOptions() {
         date = null;
         min = null;
         max = null;
         title = null;
+    }
 
-        theme = call.getString("theme", theme);
-        mode = call.getString("mode", mode);
-        format = call.getString("format", format);
-        timezone = call.getString("timezone", timezone);
-        locale = call.getString("locale",locale);
-        cancelText = call.getString("cancelText", cancelText);
-        doneText = call.getString("doneText",doneText);
-        is24h = call.getBoolean("is24h", is24h);
-        title = call.getString("title", title);
-
-        String dateStr = call.getString("date", null);
-        String minStr = call.getString("min", null);
-        String maxStr = call.getString("max", null);
-
-        if (dateStr != null) date = Parse.dateFromString(dateStr, format, timezone);
-        if (minStr != null) min = Parse.dateFromString(minStr, format, timezone);
-        if (maxStr != null) max = Parse.dateFromString(maxStr, format, timezone);
+    public Object clone() throws CloneNotSupportedException
+    {
+        return super.clone();
     }
 }
--- android/src/main/java/com/getcapacitor/community/datepicker/DatePickerPlugin.java
+++ android/src/main/java/com/getcapacitor/community/datepicker/DatePickerPlugin.java
@@ -6,16 +6,20 @@ import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
 
+import java.text.ParseException;
+
 @CapacitorPlugin(name = "DatePicker")
 public class DatePickerPlugin extends Plugin {
-
-    public static final String PLUGIN_PREFIX = "plugins.DatePicker.";
-
     public DatePickerPlugin() {}
+    DatePickerOptions options;
+
+    public void load() {
+        options = getDatePickerConfig();
+    }
 
     @PluginMethod
-    public void present(final PluginCall call) throws java.text.ParseException {
-        DatePickerOptions options = new DatePickerOptions(PLUGIN_PREFIX, getConfig(), call);
+    public void present(final PluginCall call) throws java.text.ParseException, CloneNotSupportedException {
+        DatePickerOptions options = getDatePickerConfigCall(call);
         DatePicker datePicker = new DatePicker(options, getContext());
 
         datePicker.open(
@@ -33,5 +37,43 @@ public class DatePickerPlugin extends Plugin {
                 }
             }
         );
+    }
+
+    private DatePickerOptions getDatePickerConfig() {
+        DatePickerOptions options = new DatePickerOptions();
+        options.theme = getConfig().getString("android.theme", getConfig().getString("theme", "light"));
+        options.mode = getConfig().getString("android.mode", getConfig().getString("mode", "dateAndTime"));
+        options.format = getConfig().getString("android.format", getConfig().getString("format", "yyyy-MM-dd'T'HH:mm:ss.sss"));
+        options.timezone = getConfig().getString("android.timezone", getConfig().getString("timezone", "UTC"));
+        options.locale = getConfig().getString("android.locale", getConfig().getString("locale",null));
+        options.cancelText = getConfig().getString("android.cancelText", getConfig().getString("cancelText", null));
+        options.doneText = getConfig().getString("android.doneText", getConfig().getString("doneText",null));
+        options.is24h = getConfig().getBoolean("android.is24h", getConfig().getBoolean("is24h", false));
+        return options;
+    }
+
+    private DatePickerOptions getDatePickerConfigCall(PluginCall call) throws CloneNotSupportedException, ParseException {
+
+        DatePickerOptions options = (DatePickerOptions)this.options.clone();
+
+        options.theme = call.getString("android.theme", call.getString("theme", this.options.theme));
+        options.mode = call.getString("android.mode", call.getString("mode", this.options.mode));
+        options.format = call.getString("android.format", call.getString("format", this.options.format));
+        options.timezone = call.getString("android.timezone", call.getString("timezone", this.options.timezone));
+        options.locale = call.getString("android.locale", call.getString("locale",this.options.locale));
+        options.cancelText = call.getString("android.cancelText", call.getString("cancelText", this.options.cancelText));
+        options.doneText = call.getString("android.doneText", call.getString("doneText",this.options.doneText));
+        options.is24h = call.getBoolean("android.is24h", call.getBoolean("is24h", this.options.is24h));
+        options.title = call.getString("android.title", call.getString("title", this.options.title));
+
+        String dateStr = call.getString("date", null);
+        String minStr = call.getString("min", null);
+        String maxStr = call.getString("max", null);
+
+        if (dateStr != null) options.date = Parse.dateFromString(dateStr, options.format, options.timezone);
+        if (minStr != null) options.min = Parse.dateFromString(minStr, options.format, options.timezone);
+        if (maxStr != null) options.max = Parse.dateFromString(maxStr, options.format, options.timezone);
+
+        return options;
     }
 }
