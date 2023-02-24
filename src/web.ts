83aec9c5a1453f921ca9770df1003aaf753e7748
jcesarmobile <jcesarmobile@gmail.com>
2023-02-24 20:06:51 +0100

    fix(web): ignore unused options

--- src/web.ts
+++ src/web.ts
@@ -3,7 +3,7 @@ import { WebPlugin } from '@capacitor/core';
 import type { DatePickerOptions, DatePickerPlugin } from './definitions';
 
 export class DatePickerWeb extends WebPlugin implements DatePickerPlugin {
-  present(options: DatePickerOptions): Promise<{ value: string }> {
+  present(_options: DatePickerOptions): Promise<{ value: string }> {
     throw new Error('Method not implemented.');
   }
 }
